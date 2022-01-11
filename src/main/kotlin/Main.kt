f2f9c31994876fa5d2d94afe8eaca41f0dfaf4b1
Bartosz76 <mierzwinski.b@gmail.com>
2022-01-11 23:40:59 +0100

    Added the initial draft of variables' usage including top level variables.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+//Variables defined outside of any enclosing function are top level variables.
+val name: String = "Bartosz"
+var foodBeingEaten: String = "Apple"
+//"?" is required for a variable to be a null. Types are non-null by default.
+var simpleStringsCantBeNulls: String? = null
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println(name)
+    println(foodBeingEaten)
+
+    foodBeingEaten = "Orange"
+    println(foodBeingEaten)
+
 }
