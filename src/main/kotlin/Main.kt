6c050a96ffeec08ca1919825b20441341c2fa8e5
Bartosz76 <mierzwinski.b@gmail.com>
2022-01-13 22:19:15 +0100

    Added the initial implementation of the game's menu.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,18 +5,43 @@ var foodBeingEaten: String = "Apple"
 var simpleStringsCantBeNulls: String? = null
 
 fun main() {
-    greetThePlayer()
+    acquireThePlayersName()
+    val playerName = readLine()
+    greetThePlayer(playerName)
+
+    showMenu(playerName)
+    var chosenSide = readLine()
+    provideInfoAboutTheChosenSide(chosenSide)
+
 }
 
-fun greetThePlayer() {
+fun acquireThePlayersName() {
     println("Hello to the console battle simulator.")
     println("What is your name?")
-    val playerName = readLine()
+}
 
+fun greetThePlayer(playerName: String?) {
     println("Welcome to the CBS (console battle simulator), $playerName!")
 }
 
+fun showMenu(playerName: String?) {
+    println("Which side do you prefer to play as, $playerName?")
+    Thread.sleep(1_000)
+    println("1. Dwarves")
+    println("2. Orcs")
+}
 
+fun provideInfoAboutTheChosenSide(chosenSide: String?) {
+    when (chosenSide) {
+        "1" -> dwarvesData()
+        "2" -> orcsData()
+    }
+}
 
-fun validatedPrinter() {
+fun dwarvesData() {
+    println("Khazukan Kazakit-ha!")
+}
+
+fun orcsData() {
+    println("Lok'tar ogar!")
 }
