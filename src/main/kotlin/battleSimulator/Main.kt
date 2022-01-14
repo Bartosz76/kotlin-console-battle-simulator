9e80477fec3ccef10f4df022375add93d928083c
Bartosz76 <mierzwinski.b@gmail.com>
2022-01-14 22:23:48 +0100

    Added races' descriptions to be displayed upon proper functions being called.

--- src/main/kotlin/battleSimulator/Main.kt
+++ src/main/kotlin/battleSimulator/Main.kt
@@ -29,19 +29,34 @@ fun showMenu(playerName: String?) {
     Thread.sleep(1_000)
     println("1. Dwarves")
     println("2. Orcs")
+    println("3. Elves")
 }
 
 fun provideInfoAboutTheChosenSide(chosenSide: String?) {
     when (chosenSide) {
         "1" -> dwarvesData()
         "2" -> orcsData()
+        "3" -> elvesData()
     }
 }
 
 fun dwarvesData() {
-    println("Khazukan Kazakit-ha!")
+    println("You have chosen dwarves. Hardy, stubborn and strong, they field a vast array " +
+            "of melee units with high defensive capabilities that are near impossible to break." +
+            "The deliverance from the shortcomings of slowness and low range is to arrive at the" +
+            "armoured hands of crossbowmen and boar riders.")
 }
 
 fun orcsData() {
-    println("Lok'tar ogar!")
+    println("You have chosen orcs. Brutal and ferocious, their rooster consists of strong and" +
+            "fast units that seek to engage in their speciality - hand to hand combat, as soon as" +
+            "possible. The lack of durable equipment and low morale are gaps to be filled with" +
+            "orcs with stolen shields and shamans (whom orcs fear more than any enemy).")
+}
+
+fun elvesData() {
+    println("You have chosen elves. Graceful and agile, elves specialize in ranged combat and, on a" +
+            "perfect battlefield, will harass their opponents with constant volleys of arrows, to a" +
+            "point where wardancers and swan pikemen, whose job is to make up for elves' shortcoming" +
+            "in terms of melee prowess, will not be necessary.")
 }
