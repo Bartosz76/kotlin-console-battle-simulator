16cf91d53e3369fe3b918e843bd88cb8833c5a2c
Bartosz76 <mierzwinski.b@gmail.com>
2022-02-14 22:18:40 +0100

    Converted puts to assignments in enemy army generating functions.

--- src/main/kotlin/battleSimulator/logistics/EnemyGenerator.kt
+++ src/main/kotlin/battleSimulator/logistics/EnemyGenerator.kt
@@ -11,38 +11,38 @@ class EnemyGenerator {
     internal fun generateTheEnemy(enemyFaction: String) {
         when (enemyFaction) {
             "1" -> { generateDwarvenArmy() }
-            "2" -> {}
-            "3" -> {}
+            "2" -> { generateOrcishArmy() }
+            "3" -> { generateElvenArmy() }
         }
     }
 
     private fun generateDwarvenArmy() {
-        enemyArmy.put(DwarvenUnit.SHIELDBEARERS, 6)
-        enemyArmy.put(DwarvenUnit.AXEMEN, 8)
-        enemyArmy.put(DwarvenUnit.HAMMERERS, 4)
-        enemyArmy.put(DwarvenUnit.AXETHROWERS, 8)
-        enemyArmy.put(DwarvenUnit.CROSSBOWMEN, 6)
-        enemyArmy.put(DwarvenUnit.BOAR_RIDERS, 4)
-        enemyArmy.put(DwarvenUnit.RUNE_PRIESTS, 3)
+        enemyArmy[DwarvenUnit.SHIELDBEARERS] = 6
+        enemyArmy[DwarvenUnit.AXEMEN] = 8
+        enemyArmy[DwarvenUnit.HAMMERERS] = 4
+        enemyArmy[DwarvenUnit.AXETHROWERS] = 8
+        enemyArmy[DwarvenUnit.CROSSBOWMEN] = 6
+        enemyArmy[DwarvenUnit.BOAR_RIDERS] = 4
+        enemyArmy[DwarvenUnit.RUNE_PRIESTS] = 3
     }
 
     private fun generateOrcishArmy() {
-        enemyArmy.put(OrcishUnit.ORCS_WITH_STOLEN_SHIELDS, 6)
-        enemyArmy.put(OrcishUnit.CLUBBERS, 8)
-        enemyArmy.put(OrcishUnit.BIG_AXES, 4)
-        enemyArmy.put(OrcishUnit.JAVELINERS, 8)
-        enemyArmy.put(OrcishUnit.ROCK_LOBBERS, 6)
-        enemyArmy.put(OrcishUnit.WOLF_RIDERS, 4)
-        enemyArmy.put(OrcishUnit.SHAMANS, 3)
+        enemyArmy[OrcishUnit.ORCS_WITH_STOLEN_SHIELDS] = 6
+        enemyArmy[OrcishUnit.CLUBBERS] = 8
+        enemyArmy[OrcishUnit.BIG_AXES] = 4
+        enemyArmy[OrcishUnit.JAVELINERS] = 8
+        enemyArmy[OrcishUnit.ROCK_LOBBERS] = 6
+        enemyArmy[OrcishUnit.WOLF_RIDERS] = 4
+        enemyArmy[OrcishUnit.SHAMANS] = 3
     }
 
     private fun generateElvenArmy() {
-        enemyArmy.put(ElvenUnit.WARDANCERS, 6)
-        enemyArmy.put(ElvenUnit.SWAN_PIKEMEN, 8)
-        enemyArmy.put(ElvenUnit.HUNTERS, 4)
-        enemyArmy.put(ElvenUnit.LONGBOWS, 8)
-        enemyArmy.put(ElvenUnit.ARCANE_BOWS, 6)
-        enemyArmy.put(ElvenUnit.SWAN_KNIGHTS, 4)
-        enemyArmy.put(ElvenUnit.PRIESTS_OF_HALIEL, 3)
+        enemyArmy[ElvenUnit.WARDANCERS] = 6
+        enemyArmy[ElvenUnit.SWAN_PIKEMEN] = 8
+        enemyArmy[ElvenUnit.HUNTERS] = 4
+        enemyArmy[ElvenUnit.LONGBOWS] = 8
+        enemyArmy[ElvenUnit.ARCANE_BOWS] = 6
+        enemyArmy[ElvenUnit.SWAN_KNIGHTS] = 4
+        enemyArmy[ElvenUnit.PRIESTS_OF_HALIEL] = 3
     }
 }
