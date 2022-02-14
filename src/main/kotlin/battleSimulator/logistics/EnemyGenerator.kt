0755b1f9f05bb28fe6e2bdbdb36d5d13eee39371
Bartosz76 <mierzwinski.b@gmail.com>
2022-02-14 20:01:09 +0100

    Created a separate class to generate the enemy army depending on the race chosen by the player. Implemented the generation itself for Dwarves.

--- src/main/kotlin/battleSimulator/logistics/EnemyGenerator.kt
+++ src/main/kotlin/battleSimulator/logistics/EnemyGenerator.kt
@@ -1,15 +1,27 @@
 package battleSimulator.logistics
 
+import battleSimulator.dwarves.DwarvenUnit
+
 class EnemyGenerator {
 
     private var enemyArmy = mutableMapOf<Unit, Int>()
 
     internal fun generateTheEnemy(enemyFaction: String) {
         when (enemyFaction) {
-            "1" -> {}
+            "1" -> { generateDwarvenArmy() }
             "2" -> {}
             "3" -> {}
         }
+    }
+
+    private fun generateDwarvenArmy() {
+        enemyArmy.put(DwarvenUnit.SHIELDBEARERS, 6)
+        enemyArmy.put(DwarvenUnit.AXEMEN, 8)
+        enemyArmy.put(DwarvenUnit.HAMMERERS, 4)
+        enemyArmy.put(DwarvenUnit.AXETHROWERS, 8)
+        enemyArmy.put(DwarvenUnit.CROSSBOWMEN, 6)
+        enemyArmy.put(DwarvenUnit.BOAR_RIDERS, 4)
+        enemyArmy.put(DwarvenUnit.RUNE_PRIESTS, 3)
     }
 
 }
